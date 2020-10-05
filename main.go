14fbb888310cac025de4a6f293d55595a601c3db
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-10-05 12:14:20 +0100

    Final result

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 // How to do versioning?
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -16,7 +17,7 @@ var db *gorm.DB
 var err error
 
 type Service struct {
-	gorm.model
+	gorm.Model
 	Name string
 }
 
@@ -24,19 +25,45 @@ func setupRouter(router *mux.Router) {
 	router.
 		Methods("POST").
 		Path("/services").
-		HandlerFunc(servicesCreate)
+		HandlerFunc(createService)
+
+	router.
+		Methods("GET").
+		Path("/services/{id}").
+		HandlerFunc(getService)
 }
 
-func servicesCreate(w http.ResponseWriter, r *http.Request) {
+func createService(w http.ResponseWriter, r *http.Request) {
+	var service Service
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the name")
+	}
+	if err := json.Unmarshal(reqBody, &service); err != nil {
+		panic(err)
+	}
+	db.Create(&service)
+
+	json.NewEncoder(w).Encode(service)
+}
+
+func getService(w http.ResponseWriter, r *http.Request) {
+	var service Service
+	serviceID := mux.Vars(r)["id"]
+	db.First(&service, serviceID)
+
+	json.NewEncoder(w).Encode(service)
 }
 
 func main() {
 	// Enable VIPER to read Environment Variables
 	viper.AutomaticEnv()
 
-	dbConnection := viper.GET("DATABASE_URL")
+	//	dbConnection := viper.Get("DATABASE_URL")
 
-	db, err = gorm.Open("postgres", dbConnection)
+	db, err = gorm.Open(postgres.New(postgres.Config{
+		DSN: "user=postgres password=password dbname=service_api_local host=service-api-db sslmode=disable",
+	}), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database")
@@ -46,5 +73,5 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	setupRouter(router)
-	log.Fatal(http.listenAndServe(":8888", router))
+	log.Fatal(http.ListenAndServe(":8888", router))
 }

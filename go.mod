60cd2807c03cda5223b24a26eb0e1b3ab75849ed
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-10-05 09:43:16 +0100

    Attempt 1 to connect to a db

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/tomas-stefano/playing-with-go-lang
+
+go 1.15
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/magiconair/properties v1.8.4 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/spf13/afero v1.4.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	golang.org/x/crypto v0.0.0-20201002094018-c90954cbb977 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/ini.v1 v1.61.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gorm.io/driver/postgres v1.0.2 // indirect
+	gorm.io/gorm v1.20.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
+// How to do versioning?
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Matt and Brendan!")
+var db *gorm.DB
+var err error
+
+type Service struct {
+	gorm.model
+	Name string
+}
+
+func setupRouter(router *mux.Router) {
+	router.
+		Methods("POST").
+		Path("/services").
+		HandlerFunc(servicesCreate)
+}
+
+func servicesCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	dbConnection := viper.GET("DATABASE_URL")
+
+	db, err = gorm.Open("postgres", dbConnection)
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	db.AutoMigrate(&Service{})
+
+	router := mux.NewRouter().StrictSlash(true)
+	setupRouter(router)
+	log.Fatal(http.listenAndServe(":8888", router))
 }
